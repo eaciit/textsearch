ed53a7e2748f8615b6301f4e3af9fe2951577d34
juragan360 <ariefda@hotmail.com>
2015-10-21 04:52:59 +0700

    change testing way

--- soundex_test.go
+++ soundex_test.go
@@ -6,15 +6,17 @@ import (
 )
 
 func Test_Soundex(t *testing.T) {
+	fmt.Printf("\nTest Soundex\n")
 	diff := SoundexDiff("Canon", "Canyon")
 	if diff >= 80 {
 		fmt.Println("OK, both words are similar")
 	} else {
-		fmt.Printf("Sorry but both words are different. Similarity index is: %d \n", diff)
+		t.Errorf("Sorry but both words are different. Similarity index is: %d \n", diff)
 	}
 }
 
 func Test_TextSimilarity(t *testing.T) {
+	fmt.Printf("\nTest Similarity\n")
 	s1 := "My name is Arief Darmawan"
 	s2 := "Arf Darmawan is the name"
 	setting := NewSimilaritySetting()
@@ -24,6 +26,6 @@ func Test_TextSimilarity(t *testing.T) {
 	if similar >= 80 {
 		fmt.Println("OK, both sentences are similar")
 	} else {
-		fmt.Printf("Sorry but both sentences are different. Similarity index is: %d \n", similar)
+		t.Errorf("Sorry but both sentences are different. Similarity index is: %d \n", similar)
 	}
 }
